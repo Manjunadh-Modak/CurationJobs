e42003971f531ab3b7ef14219fbc54fdbdfc7a41
Manjunadh <125258588+Manjunadh-Modak@users.noreply.github.com>
2023-05-22 16:12:58 +0530

    Create lookupcloud.py

---
+++ lookupcloud.py
@@ -0,0 +1,14 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.config("spark.jars", "/home/ma0804/Downloads/postgresql-42.4.2.jar") \
+    .master("local").appName("Pyspark").getOrCreate()
+
+df = spark.read.format("jdbc").option("url", "jdbc:postgresql://172.30.3.196:5432/yeedu") \
+    .option("driver", "org.postgresql.Driver").option("dbtable", "yeedu.lookup_cloud_providers") \
+    .option("user", "postgres").option("password", "postgres").load()
+
+df.printSchema()
+df.select("id","name","description").show()
+
+df.write.mode("Overwrite").option("path", "/cdpmodakbucket/cdpdevenv/data/warehouse/tablespace/external/hive/yeedu/lookup_cloud_provider.parquet")
