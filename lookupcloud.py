94dc95f306d059633005f8d938f244f1fb132773
Manjunadh <125258588+Manjunadh-Modak@users.noreply.github.com>
2023-05-24 15:23:10 +0530

    Update lookupcloud.py

--- lookupcloud.py
+++ lookupcloud.py
@@ -2,8 +2,9 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from pyspark.sql import Row
+from pyspark.conf import SparkConf
 
-spark = SparkSession.builder.config("spark.jars", "/home/ma0804/Downloads/postgresql-42.4.2.jar").master("local").appName("Pyspark").getOrCreate()
+spark = SparkSession.builder.master("local").appName("Pyspark").getOrCreate()
 
 df = spark.read.format("jdbc").option("url", "jdbc:postgresql://172.30.3.196:5432/yeedu") \
     .option("driver", "org.postgresql.Driver").option("query", "SELECT id,name,description,CASE WHEN from_date IS NULL THEN '' ELSE to_char(from_date, 'MM-DD-YYYY HH24:MI:SS') END AS from_date,CAST(to_date AS text) as to_date FROM yeedu.lookup_cloud_providers") \
