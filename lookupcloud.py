1fdbc58e60a2cdb525c988e618f120159c1e3cc0
Manjunadh <125258588+Manjunadh-Modak@users.noreply.github.com>
2023-05-24 18:40:13 +0530

    Update lookupcloud.py

--- lookupcloud.py
+++ lookupcloud.py
@@ -6,7 +6,7 @@ spark = SparkSession.builder.master("local").appName("Pyspark").getOrCreate()
 table_name=spark.sparkContext.getConf().get("spark.driver.lookup_value")
 
 df = spark.read.format("jdbc").option("url", "jdbc:postgresql://172.30.3.196:5432/yeedu") \
-    .option("driver", "org.postgresql.Driver").option("query", f"SELECT id,name,description,CAST(from_date as timestamp) FROM yeedu.{table_name}") \
+    .option("driver", "org.postgresql.Driver").option("query", f"SELECT * FROM yeedu.{table_name}") \
     .option("user", "postgres").option("password", "postgres").load()
 
 df.show(truncate=False)
