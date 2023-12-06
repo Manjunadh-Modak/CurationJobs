b0ce259de440da79801a72e67e499c5c43e81b7d
Manjunadh <125258588+Manjunadh-Modak@users.noreply.github.com>
2023-12-06 20:23:04 +0530

    Create sample.scala

---
+++ sample.scala
@@ -0,0 +1,21 @@
+import com.github.music.of.the.ainur.almaren.builder.Core.Implicit
+import com.github.music.of.the.ainur.almaren.Almaren
+ 
+
+val almaren = Almaren("App Name")
+val someDF = Seq(
+  (1, 1),
+  (64, 64),
+  (-27, 27)
+).toDF("datastore_id", "table_id")
+someDF.show(false)
+var a = 1000000
+ 
+while (a > 0) 
+        {
+            println("a is : " + a)
+            a = a - 1;
+            Thread.sleep(3000)
+            val df = almaren.builder.sourceDataFrame(someDF).batch
+            df.show(false)
+        }
